501f00150cd43cc58b688091e9e26307ae47e4fb
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2020-01-28 15:03:39 +0000

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libraryApiClient';
+
+  response$: Observable<BooksResponse>;
+
+  constructor(private client: HttpClient) {}
+
+  getThem() {
+    this.response$ = this.client.get<BooksResponse>('http://localhost:1337/books'); // .subscribe(r => console.log(r));
+  }
+}
+
+interface BooksResponse {
+  data: BookResponseItem;
+}
+
+interface BookResponseItem {
+  id: number;
+  title: string;
+  author: string;
 }
